2dd8dd63b98b0f9ae0e97b084cc0aa56da2f72e4
eshansaif <eshansaif1234@gmail.com>
2024-06-21 00:37:37 +0600

    stats dashboard updated

--- src/pages/dashboard/StatsDashboard.jsx
+++ src/pages/dashboard/StatsDashboard.jsx
@@ -132,22 +132,100 @@ const StatsDashboard = () => {
   };
 
   return (
-    <div className="overflow-x-auto w-full px-16 mt-10">
+    <div className="overflow-x-auto w-full px-4 md:px-16 mt-10">
       <h1 className="text-center font-mono font-bold text-3xl underline">
         Statistics of the System
       </h1>
-      <div className="grid md:grid-cols-2">
-        <div className="mb-12">
-          <Bar data={barData} options={{ responsive: true }} />
-          <p className="font-mono font-thin underline text-center">
-            Total Counts
-          </p>
+      <div className="my-10 space-y-5">
+        <div className="stats shadow grid md:grid-cols-6">
+          <div className="stat">
+            <div className="stat-figure text-primary"></div>
+            <div className="stat-title">Total Users</div>
+            <div className="stat-value text-primary">{totalUsers}</div>
+          </div>
+          <div className="stat">
+            <div className="stat-figure textarea-info"></div>
+            <div className="stat-title">Total Admin</div>
+            <div className="stat-value textarea-info">{totalAdmins}</div>
+          </div>
+          <div className="stat">
+            <div className="stat-figure text-error"></div>
+            <div className="stat-title">Total Students</div>
+            <div className="stat-value text-error">{totalStudents}</div>
+          </div>
+          <div className="stat">
+            <div className="stat-figure text-warning"></div>
+            <div className="stat-title">Total Parents</div>
+            <div className="stat-value text-warning">{totalParents}</div>
+          </div>
+          <div className="stat">
+            <div className="stat-figure text-success"></div>
+            <div className="stat-title">Total Active Users</div>
+            <div className="stat-value text-success">{activeUsersCount}</div>
+          </div>
+          <div className="stat">
+            <div className="stat-figure text-secondary"></div>
+            <div className="stat-title">Total Deleted Users</div>
+            <div className="stat-value text-secondary">{deletedUsersCount}</div>
+          </div>
         </div>
-        <div className="mb-12">
-          <Pie data={pieData} options={{ responsive: true }} />
-          <p className="font-mono font-thin underline text-center">
-            Location Counts Added By EduMap Chemnitz
-          </p>
+        <div className="stats shadow grid md:grid-cols-5">
+          <div className="stat">
+            <div className="stat-figure text-primary"></div>
+            <div className="stat-title">Total Locations</div>
+            <div className="stat-value text-primary">
+              {schoolCount +
+                kindergartenCount +
+                socialChildProjectsCount +
+                socialTeenagerProjectsCount}
+            </div>
+          </div>
+          <div className="stat">
+            <div className="stat-figure text-info"></div>
+            <div className="stat-title">Total Schools</div>
+            <div className="stat-value text-info">{schoolCount}</div>
+          </div>
+          <div className="stat">
+            <div className="stat-figure text-error"></div>
+            <div className="stat-title">Total Kindergartens</div>
+            <div className="stat-value text-error">{kindergartenCount}</div>
+          </div>
+          <div className="stat">
+            <div className="stat-figure text-success"></div>
+            <div className="stat-title">Social Child Projects</div>
+            <div className="stat-value text-primary">
+              {socialChildProjectsCount}
+            </div>
+          </div>
+          <div className="stat">
+            <div className="stat-figure text-secondary"></div>
+            <div className="stat-title">Social Teenager Projects</div>
+            <div className="stat-value text-secondary">
+              {socialTeenagerProjectsCount}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="grid md:grid-cols-2 gap-4">
+        <div className="w-full mb-12">
+          <div className="bg-white rounded-lg shadow p-4">
+            <div className="relative h-64 md:h-96">
+              <Bar data={barData} options={{ responsive: true }} />
+            </div>
+            <p className="font-mono font-thin underline text-center mt-4">
+              Total Counts
+            </p>
+          </div>
+        </div>
+        <div className="w-full mb-12">
+          <div className=" rounded-lg ">
+            <div className="relative h-64 md:h-96">
+              <Pie data={pieData} options={{ responsive: true }} />
+            </div>
+            <p className="font-mono font-thin underline text-center mt-4">
+              Location Counts Added By EduMap Chemnitz
+            </p>
+          </div>
         </div>
       </div>
     </div>
